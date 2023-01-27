4b61febb799f6a129097b34732a2a6830bded079
jayanthmahadesh <jayanthmahadesh777@gmail.com>
2023-01-27 23:22:11 +0530

    third

--- fitness/urls.py
+++ fitness/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Login, SignUp, Index
+from .views import Login, SignUp, Index,outdoor
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', Index.as_view(), name='indexpage'),
     path('login', Login.as_view(), name='login'),
     path('signup', SignUp.as_view(), name='signup'),
-
+    path('outdoor',outdoor.as_view(),name='outdoor'),
 ]
